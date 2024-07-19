d05ae0b74e3743205d67efe864d9ba4ad40bdd71
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-07-19 09:40:03 +0200

    commit initial (main.py, nim_games.py)

--- nim_games.py
+++ nim_games.py
@@ -1,1 +1,6 @@
-print("j'ai gagné !")
+def victoire():
+    """Ceci est un programme pour jouer au jeu de Nim taquin
+    et non un programme pour jouer au taquin à Nîmes"""
+    print("j'ai gagné !")
+
+victoire()
